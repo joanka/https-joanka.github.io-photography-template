192bec75de4bfead1baae2f0fa4a77d075995759
joanka <joasiek136@gmail.com>
2018-01-10 00:25:03 +0100

    made header section responsive

---
+++ app/js/index.js
@@ -0,0 +1,17 @@
+(function() {
+	var button = document.querySelector('.nav-toggler');
+	var overlay = document.querySelector('.nav__overlay');
+	var nav = document.querySelector('.nav__list');
+
+	button.addEventListener('click', function(e) {			
+        nav.classList.toggle('nav-open');
+        overlay.classList.toggle('overlay-open');
+		button.setAttribute('aria-expanded', String( nav.classList.contains( 'nav-open')));
+	}, false );
+
+	nav.addEventListener('click', function(e) {
+		nav.classList.toggle( 'nav-open' );
+        overlay.classList.toggle( 'overlay-open' );
+	}, false);
+
+}());
