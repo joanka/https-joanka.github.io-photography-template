e49decaccd48ee2dea43c701a59c2cfa819feb1d
joanka <joasiek136@gmail.com>
2018-02-09 00:21:38 +0100

    added sticky nav and backToTop btn

--- app/js/index.js
+++ app/js/index.js
@@ -1,9 +1,11 @@
 (function() {
 	'use strict';
 
-	var button = document.querySelector('.nav-toggler');
-	var overlay = document.querySelector('.nav__overlay');
-	var nav = document.querySelector('.nav__list');
+	const button = document.querySelector('.nav-toggler');
+	const overlay = document.querySelector('.nav__overlay');
+	const nav = document.querySelector('.nav__list');
+	const chevron = document.querySelector('.header__chevron');
+	const form  = document.querySelector(".message-form");
 
 	button.addEventListener('click', function(e) {			
         nav.classList.toggle('nav-open');
@@ -16,13 +18,51 @@
         overlay.classList.toggle( 'overlay-open' );
 	}, false);
 
-	//form
-	var form  = document.querySelector(".message-form");
-	var inputs = document.querySelectorAll('.message-form__elem');
+	//backToTop btn
+	function createBtn() {
+		const btnToTop = document.createElement("button");
+		btnToTop.classList.add("btn--top", "hidden");
+		btnToTop.innerHTML = "&#xbb;";
+		document.body.appendChild(btnToTop);
+		return btnToTop;
+	}
 
-	form.onSubmit = function(e) {
-		e.preventDefault();
-	};
+	const btnToTop = createBtn();
+
+	window.addEventListener("scroll", function(e) {
+		if(window.pageYOffset || document.documentElement.scrollTop >= 100) {
+			btnToTop.classList.remove("hidden");
+			chevron.classList.add("hidden");
+		} else {
+			btnToTop.classList.add("hidden");
+			chevron.classList.remove("hidden");
+		} 
+	}, false);
+
+	function animateScrollTop() {
+		if(window.pageYOffset || document.documentElement.scrollTop > 0) {
+			window.scrollBy(0, -60);
+			setTimeout(animateScrollTop, 10);
+		}
+	}
+
+	btnToTop.addEventListener("click",function(e) {
+		e.stopPropagation();
+		animateScrollTop();
+	}, false);
+
+	//sticky nav
+	function stickyNavigation() {	  
+		if (window.scrollY || window.pageYOffset > 80) {
+		  document.body.classList.add('fixed-nav');
+		} else {
+		  document.body.classList.remove('fixed-nav');
+		}
+	}
+
+	window.addEventListener('scroll', stickyNavigation);
+
+
 
 
 }());
